3ae36b8786a60ff2bcc3fca0ac9c3b717ded1b2f
duguowei1000 <duguowei1000@gmail.com>
2022-12-24 23:34:45 +0800

    next env variables

--- pages/positions.tsx
+++ pages/positions.tsx
@@ -1,14 +1,17 @@
 
 import { useEffect, useState } from "react";
 
-const chain  = 'ethereum'
-const associatedaddr = '0x14e6b828AdB52153E5BF7C740A1a7312ef4B8711'
+const chain  = 'ethereum' //'avalanche'//
+const associatedaddr = process.env.NEXT_PUBLIC_associatedaddr 
+const ZAPPER_API_KEY = process.env.NEXT_PUBLIC_ZAPPER_API_KEY
+const appID ="aave-v2"
+const fetchURL =`https://api.zapper.fi/v2/apps/${appID}/balances?addresses%5B%5D=${associatedaddr}&network=${chain}&api_key=${ZAPPER_API_KEY}`//`https://api.zapper.fi/v2/balances?addresses%5B%5D=${associatedaddr}&networks%5B%5D=${chain}&api_key=${ZAPPER_API_KEY}``
+console.log('fetchURL',fetchURL)
+
 function Positions(props) {
     const [status, setStatus] = useState()
 
-    const ZAPPER_API_KEY = 'b6c41b98-a976-4243-ad1c-987542eb68da'  
-
-    //TO TEST FETCH OUTPUT
+ 
     async function safeParseJSON(response) {
         const body = await response.json();
         try {
@@ -20,23 +23,19 @@ function Positions(props) {
             // throw err;
         }
     }
-
     useEffect(() => {
+
         setStatus("loading")
-        fetch('https://api.zapper.fi/v2/apps/llama-airforce/balances?addresses%5B%5D=0x14e6b828AdB52153E5BF7C740A1a7312ef4B8711&networks%5B%5D=ethereum&api_key=b6c41b98-a976-4243-ad1c-987542eb68da'//`https://api.zapper.fi/v2/balances?addresses%5B%5D=${associatedaddr}&networks%5B%5D=${chain}&api_key=${ZAPPER_API_KEY}`   
-          )
-            .then((response) => //IF YOU BRACKET IT , IT DOESNT WORK
-                response.json()
-            //}
-            )
-            .then((dailyprices) => {
-                console.log('dailyprices',dailyprices)
+        fetch(fetchURL)   
+          
+            .then((response) => response.json())
+            .then((data) => {
+                console.log('data',data)
                 setStatus("success")
             })
             .catch((error) => {
                 setStatus("error")
                 console.error("Error:", error);
-
                 // throw err;
                 return error.message;
             })
