c50ddca885d149a1c6e85658403accff04b8da29
duguowei1000 <duguowei1000@gmail.com>
2022-12-24 22:27:38 +0800

    test fetch

--- pages/positions.tsx
+++ pages/positions.tsx
@@ -6,20 +6,6 @@ const associatedaddr = '0x14e6b828AdB52153E5BF7C740A1a7312ef4B8711'
 function Positions(props) {
     const [status, setStatus] = useState()
 
-    // const generateEventSourceDict = () => {
-    //     return {
-    //       withCredentials: true,
-    //       headers: {
-    //         "Content-Type": "text/event-stream",
-    //         "User-Agent": "Mozilla/5.0",
-    //         Authorization: `Basic ${Buffer.from(
-    //           `${ZAPPER_API_KEY}:`,
-    //           "binary"
-    //         ).toString("base64")}`,
-    //       },
-    //     };
-    //   };
-      //https://api.zapper.fi/v2/balances?addresses%5B%5D=0x14e6b828AdB52153E5BF7C740A1a7312ef4B8711&networks%5B%5D=ethereum&api_key=b6c41b98-a976-4243-ad1c-987542eb68da
     const ZAPPER_API_KEY = 'b6c41b98-a976-4243-ad1c-987542eb68da'  
 
     //TO TEST FETCH OUTPUT
@@ -34,35 +20,26 @@ function Positions(props) {
             // throw err;
         }
     }
-    
-    //const json = await = fetch(...).then(safeParseJSON)
 
     useEffect(() => {
         setStatus("loading")
         fetch('https://api.zapper.fi/v2/apps/llama-airforce/balances?addresses%5B%5D=0x14e6b828AdB52153E5BF7C740A1a7312ef4B8711&networks%5B%5D=ethereum&api_key=b6c41b98-a976-4243-ad1c-987542eb68da'//`https://api.zapper.fi/v2/balances?addresses%5B%5D=${associatedaddr}&networks%5B%5D=${chain}&api_key=${ZAPPER_API_KEY}`   
           )
-          .then((response)=> {
-           safeParseJSON(response)
-        //   const raw = response.json()
-        //   console.log("raw[0].data",raw[0].data)
-    }
-          )
-            // .then((response) => {
-            //     const body = response.json()//response.text();
-            //         console.log('body',body)
-            //         // JSON.parse(body)
-            // })
-            // .then((dailyprices) => {
-            //     console.log('dailyprices',dailyprices)
-            //     setStatus("success")
-            // })
-            // .catch((error) => {
-            //     setStatus("error")
-            //     console.error("Error:", error);
+            .then((response) => //IF YOU BRACKET IT , IT DOESNT WORK
+                response.json()
+            //}
+            )
+            .then((dailyprices) => {
+                console.log('dailyprices',dailyprices)
+                setStatus("success")
+            })
+            .catch((error) => {
+                setStatus("error")
+                console.error("Error:", error);
 
-            //     // throw err;
-            //     return error.message;
-            // })
+                // throw err;
+                return error.message;
+            })
 
     }, []);
 
