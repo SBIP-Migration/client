de1dcad345da5b21b352383f7017262fb9d612d0
Matthew <alexandermatthew12@gmail.com>
2023-01-01 15:38:29 +0700

    Add ability to approve each token

--- pages/components/Balances.tsx
+++ pages/components/Balances.tsx
@@ -1,17 +1,20 @@
-import { ethers } from 'ethers'
+import { BigNumber, ethers } from 'ethers'
 import { useEffect, useState } from 'react'
-import { TOKEN_LIST } from '../Tokenlist'
-import { Flex, Text } from '@chakra-ui/react'
+import { Button, Flex, Text } from '@chakra-ui/react'
 import { useWallets } from '@web3-onboard/react'
-import { getWeb3Provider } from '../../utils/ethers'
+import { approveToken, getWeb3Provider } from '../../utils/ethers'
+import { AAVE_MIGRATION_CONTRACT } from '../../constants'
+import { getATokenBalances } from '../../utils/balances'
 
 type Props = {}
 
-type aTokenType = {
+export type aTokenType = {
   symbol: string
   contractAddress: string
-  allowance?: string
-  balance?: string
+
+  allowance: BigNumber
+  balance: BigNumber
+
   tokenAddress: string
   balanceInTokenDecimals: string
 }
@@ -22,83 +25,44 @@ const Balances = ({}: Props) => {
   const provider = getWeb3Provider(wallet)
   const walletSigner = wallet.accounts?.[0].address
 
-  //   useEffect(() => {
-  //     if(imported.walletSigner){
-  //         console.log("address_Imported",address_Imported)
-  //         console.log("props.provider>>>>>",props.provider)
-  //         getAllBalances(address_Imported)
-
-  //     }
-
-  //   },[address_Imported ])//stableDebtBalances, variableDebtBalances, current]);
   useEffect(() => {
     async function getAllBalances(address: string) {
-      await getATokenBalances(address)
-      // await getStableDebtBalances(address1);
-      // await getVariableDebtBalances(address1);
+      const aTokenBalances = await getATokenBalances(provider, address)
+      setATokenBalances(aTokenBalances)
+      // await getStableDebtBalances(address);
+      // await getVariableDebtBalances(address);
     }
 
-    if (walletSigner.length && provider != null) {
-      getAllBalances(walletSigner)
-    }
-
-    async function getATokenBalances(address: string) {
-      const erc20Abi = [
-        'function balanceOf(address account) view returns (uint256)',
-      ]
-      let callList = []
-      const aTokenList = []
-      for (let i = 0; i < TOKEN_LIST.length; i++) {
-        aTokenList.push(TOKEN_LIST[i].aTokenAddress)
+    ;(async () => {
+      if (walletSigner.length && provider != null) {
+        await getAllBalances(walletSigner)
       }
-
-      for (let i = 0; i < aTokenList.length; i++) {
-        const tokenContract = new ethers.Contract(
-          aTokenList[i],
-          erc20Abi,
-          provider
-        )
-        const tokenBalanceCall = await tokenContract.balanceOf(address)
-        const bigNumber = parseInt(tokenBalanceCall) // 29803630.997051883414242659
-        // const etherutils = ethers.utils.formatUnits(bigNumber.toString(), TOKEN_LIST[i].decimals)
-        callList.push(bigNumber)
-        console.log('calllist', callList)
-      }
-
-      const aTokenBalancesList: Array<aTokenType> = []
-      for (let i = 0; i < aTokenList.length; i++) {
-        // if (!aTokenBalancesHex[i].eq(0)) {
-        //     // let newBalance = ethers.BigNumber.from(aTokenBalancesHex[i]);
-        //     // if (newBalance.lt(100000)) {
-        //     //     console.log('Balance too low to be stored');
-        //     //     newBalance = ethers.BigNumber.from('0');
-        //     // } else {
-        //     //     newBalance = newBalance.sub(newBalance.div(100000));
-        //     // }
-
-        aTokenBalancesList.push({
-          symbol: `a${TOKEN_LIST[i].symbol}`,
-          contractAddress: TOKEN_LIST[i].aTokenAddress,
-          tokenAddress: TOKEN_LIST[i].tokenAddress,
-          // balance: newBalance.toString(),
-          allowance: undefined,
-          balanceInTokenDecimals: ethers.utils.formatUnits(
-            callList[i].toString(),
-            TOKEN_LIST[i].decimals
-          ),
-        })
-      }
-      setATokenBalances(aTokenBalancesList)
-    }
+    })()
   }, [walletSigner, provider])
+
+  const onHandleApprove = async (token: aTokenType) => {
+    await approveToken(
+      wallet,
+      AAVE_MIGRATION_CONTRACT,
+      token.contractAddress,
+      // Maximum amount of tokens that can be approved -> 2^256 - 1
+      BigNumber.from(ethers.BigNumber.from(2).pow(255))
+    )
+  }
 
   return (
     <Flex flexDir="column">
       {aTokenBalances.map((el) => (
-        <Flex key={el.symbol}>
-          <Text>
+        <Flex key={el.symbol} alignItems="center" mb="3">
+          <Text mr="3">
             {el.symbol}: {el.balanceInTokenDecimals}{' '}
           </Text>
+          <Button
+            disabled={!!el.allowance.gt(el.balance)}
+            onClick={() => onHandleApprove(el)}
+          >
+            {el.allowance.gt(el.balance) ? 'Approved' : 'Approve'}
+          </Button>
         </Flex>
       ))}
     </Flex>
--- utils/ethers.ts
+++ utils/ethers.ts
@@ -1,8 +1,32 @@
 import { WalletState } from '@web3-onboard/core'
-import { ethers } from 'ethers'
+import { BigNumber, ethers } from 'ethers'
+import { erc20ABI } from 'wagmi'
+
+let provider: ethers.providers.Web3Provider
 
 const getWeb3Provider = (wallet: WalletState) => {
-  return new ethers.providers.Web3Provider(wallet.provider)
+  if (!provider) {
+    provider = new ethers.providers.Web3Provider(wallet.provider)
+  }
+  return provider
 }
 
-export { getWeb3Provider }
+const approveToken = async (
+  wallet: WalletState,
+  contractAddress: string,
+  tokenAddress: string,
+  amount: BigNumber
+) => {
+  const provider = getWeb3Provider(wallet)
+  const signer = provider.getSigner()
+
+  if (!signer) {
+    throw new Error('No signer')
+  }
+
+  const erc20Contract = new ethers.Contract(tokenAddress, erc20ABI, signer)
+
+  await erc20Contract.approve(contractAddress, amount)
+}
+
+export { getWeb3Provider, approveToken }
