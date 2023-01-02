0eaf9cba7899da9b2e4da62a3bd889a5295e4895
duguowei1000 <duguowei1000@gmail.com>
2023-01-02 20:40:33 +0800

    added step-progress ui without desceription

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,6 +17,7 @@ import {
   getVariableDebtBalances,
 } from '../utils/balances'
 import { ethers } from 'ethers'
+import Step_progess from './stepprogress/step_progress'
 
 const injected = injectedModule()
 
@@ -140,6 +141,7 @@ export default function Home() {
           {wallet && (
             <VStack>
               <Text size="md">Address connected: {walletSigner} </Text>
+              <Step_progess/>
               <Balances
                 refreshTokenBalances={() => getAllBalances(walletSigner)}
                 aTokenBalances={aTokenBalances}
---
+++ pages/stepprogress/step.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button, Flex, Heading, Text, VStack } from '@chakra-ui/react'
+
+export default function Step(props) {
+    return (
+        <div className={"stepBlock" + (props.selected ? " selected" : "")}>
+            <div className="circleWrapper" onClick={() => props.updateStep(props.index + 1)}>
+            <div className="circle">{props.index + 1}</div>
+            </div>
+            
+            <span>{props.label}</span>
+        </div>
+    )
+}
